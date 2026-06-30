15a86768e724c1b7312e00cb9d1fd6502982d413
Ramiro F <ramirofernandez244@gmail.com>
2026-06-29 23:25:07 -0700

    - Added "background image" for Farm map

---
+++ res/worlds/test/firstFloorFarm.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="firstFloorFarm" tilewidth="3000" tileheight="1700" tilecount="1" columns="1">
+ <image source="../map/firstFloorFarm.png" width="3000" height="1750"/>
+</tileset>
