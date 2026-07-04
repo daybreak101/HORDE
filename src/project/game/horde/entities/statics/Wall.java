6c1c0d6504daf87962d4a8ca82c43d3a674197c8
Ramiro F <ramirofernandez244@gmail.com>
2026-07-04 00:00:08 -0700

    - Attempted to optimize walls for slower PC's

--- src/project/game/horde/entities/statics/Wall.java
+++ src/project/game/horde/entities/statics/Wall.java
@@ -14,6 +14,13 @@ public class Wall extends InteractableStaticEntity {
     int whatWall;
     boolean wallVisible;
     boolean showCoords;
+    private final Point2D.Float p1, p2, p3, p4;
+    private final Point2D.Float poly1 = new Point2D.Float();
+    private final Point2D.Float poly2 = new Point2D.Float();
+    private final Point2D.Float poly3 = new Point2D.Float();
+    private final Point2D.Float poly4 = new Point2D.Float();
+
+    private final Polygon trapezoid = new Polygon();
 
     // 0 = horizontal up
     // 1 = horizontal down
@@ -55,6 +62,13 @@ public class Wall extends InteractableStaticEntity {
 
         wallVisible = true;
         showCoords = false;
+
+        p1 = new Point2D.Float(x, y); // top left
+        p2 = new Point2D.Float(x + bounds.width, y); // top right
+        p3 = new Point2D.Float(x, y + bounds.height); // bottom left
+        p4 = new Point2D.Float(x + bounds.width, y + bounds.height); // bottom right
+        // poly 1 and 2 are points on the wall, 3 and 4 are world borders
+
     }
 
     @Override
@@ -65,14 +79,7 @@ public class Wall extends InteractableStaticEntity {
                 (int) (y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
 
         if (wallVisible) {
-            switch (whatWall) {
-                case 1:
-                    renderPeripherals3(g, handler.getCurrentPlayer());
-                    break;
-                case 3:
-                    renderPeripherals3(g, handler.getCurrentPlayer());
-                    break;
-            }
+            renderPeripherals3(g, handler.getCurrentPlayer());
         }
         if (showCoords) {
             //corner 1
@@ -95,10 +102,11 @@ public class Wall extends InteractableStaticEntity {
         }
     }
 
-
     public int whatWall() {
         return whatWall;
     }
+
+    float tanOfAngle, oppositeLength;
 
     public void renderPeripherals3(Graphics g, Player player) {
         g.setColor(new Color(30, 50, 40, 255));
@@ -106,158 +114,148 @@ public class Wall extends InteractableStaticEntity {
         // intialize useful points
         float playerX = player.getCenterX();
         float playerY = player.getCenterY();
-        Point2D.Float p1 = new Point2D.Float(x, y), // top left
-                p2 = new Point2D.Float(x + bounds.width, y), // top right
-                p3 = new Point2D.Float(x, y + bounds.height), // bottom left
-                p4 = new Point2D.Float(x + bounds.width, y + bounds.height), // bottom right
-                center = null, // center point based on player y
-                // poly 1 and 2 are points on the wall, 3 and 4 are world borders
-                poly1 = null, poly2 = null, poly3 = null, poly4 = null;
-
-        // find polygon points
-        float tanOfAngle, oppositeLength;
 
         if (playerY < y) {
             // quadrent 1
             if (playerX < x) {
-                poly1 = p3;
-                poly2 = p2;
+                poly1.setLocation(p3);
+                poly2.setLocation(p2);
             } // quadrent 2
             else if (playerX >= x && playerX <= x + bounds.width) {
-                poly1 = p1;
-                poly2 = p2;
+                poly1.setLocation(p1);
+                poly2.setLocation(p2);
             } // quadrent 3
             else if (playerX > x) {
-                poly1 = p1;
-                poly2 = p4;
+                poly1.setLocation(p1);
+                poly2.setLocation(p4);
             }
             if (poly1 == null) {
-                poly1 = p1;
+                poly1.setLocation(p1);
             }
             if (poly2 == null) {
-                poly2 = p2;
+                poly2.setLocation(p2);
             }
             if (poly3 == null) {
-                poly3 = p3;
+                poly3.setLocation(p3);
             }
             if (poly4 == null) {
-                poly4 = p4;
+                poly4.setLocation(p4);
             }
             tanOfAngle = (poly2.x - playerX) / (poly2.y - playerY);
             oppositeLength = (handler.getWorld().getHeight() * 100 - playerY) * tanOfAngle;
-            poly3 = new Point2D.Float(playerX + oppositeLength, handler.getWorld().getHeight() * 100);
+            poly3.setLocation(playerX + oppositeLength, handler.getWorld().getHeight() * 100);
 
             tanOfAngle = (playerX - poly1.x) / (poly1.y - playerY);
             oppositeLength = (handler.getWorld().getHeight() * 100 - playerY) * tanOfAngle;
-            poly4 = new Point2D.Float(playerX - oppositeLength, handler.getWorld().getHeight() * 100);
+            poly4.setLocation(playerX - oppositeLength, handler.getWorld().getHeight() * 100);
 
         } else if (playerY >= y && playerY <= y + bounds.height) {
             // quadrent 4
             if (playerX < x) {
-                poly1 = p3;
-                poly2 = p1;
+                poly1.setLocation(p3);
+                poly2.setLocation(p1);
 
                 if (poly1 == null) {
-                    poly1 = p1;
+                    poly1.setLocation(p1);
                 }
                 if (poly2 == null) {
-                    poly2 = p2;
+                    poly2.setLocation(p2);
                 }
                 if (poly3 == null) {
-                    poly3 = p3;
+                    poly3.setLocation(p3);
                 }
                 if (poly4 == null) {
-                    poly4 = p4;
+                    poly4.setLocation(p4);
                 }
 
                 tanOfAngle = (playerY - poly1.y) / (poly1.x - playerX);
                 oppositeLength = (handler.getWorld().getWidth() * 100 - playerX) * tanOfAngle;
-                poly4 = new Point2D.Float(handler.getWorld().getWidth() * 100, playerY - oppositeLength);
+                poly4.setLocation(handler.getWorld().getWidth() * 100, playerY - oppositeLength);
 
                 tanOfAngle = (poly2.y - playerY) / (poly2.x - playerX);
                 oppositeLength = (handler.getWorld().getWidth() * 100 - playerX) * tanOfAngle;
-                poly3 = new Point2D.Float(handler.getWorld().getWidth() * 100, playerY + oppositeLength);
+                poly3.setLocation(handler.getWorld().getWidth() * 100, playerY + oppositeLength);
 
             } // quadrent 5
             else if (playerX > x) {
-                poly1 = p2;
-                poly2 = p4;
+                poly1.setLocation(p2);
+                poly2.setLocation(p4);
 
                 if (poly1 == null) {
-                    poly1 = p1;
+                    poly1.setLocation(p1);
                 }
                 if (poly2 == null) {
-                    poly2 = p2;
+                    poly2.setLocation(p2);
                 }
                 if (poly3 == null) {
-                    poly3 = p3;
+                    poly3.setLocation(p3);
                 }
                 if (poly4 == null) {
-                    poly4 = p4;
+                    poly4.setLocation(p4);
                 }
 
                 tanOfAngle = (poly1.y - playerY) / (playerX - poly1.x);
                 oppositeLength = (playerX) * tanOfAngle;
-                poly4 = new Point2D.Float(0, playerY + oppositeLength);
+                poly4.setLocation(0, playerY + oppositeLength);
 
                 tanOfAngle = (playerY - poly2.y) / (playerX - poly2.x);
                 oppositeLength = (playerX) * tanOfAngle;
-                poly3 = new Point2D.Float(0, playerY - oppositeLength);
+                poly3.setLocation(0, playerY - oppositeLength);
 
             }
 
         } else if (playerY > y + bounds.height) {
             // quadrent 6
             if (playerX < x) {
-                poly1 = p4;
-                poly2 = p1;
+                poly1.setLocation(p4);
+                poly2.setLocation(p1);
             } // quadrent 7
             else if (playerX >= x && playerX <= x + bounds.width) {
-                poly1 = p4;
-                poly2 = p3;
+                poly1.setLocation(p4);
+                poly2.setLocation(p3);
             } // quadrent 8
             else if (playerX > x + bounds.width) {
-                poly1 = p2;
-                poly2 = p3;
+                poly1.setLocation(p2);
+                poly2.setLocation(p3);
             }
 
             if (poly1 == null) {
-                poly1 = p1;
+                poly1.setLocation(p1);
             }
             if (poly2 == null) {
-                poly2 = p2;
+                poly2.setLocation(p2);
             }
             if (poly3 == null) {
-                poly3 = p3;
+                poly3.setLocation(p3);
             }
             if (poly4 == null) {
-                poly4 = p4;
+                poly4.setLocation(p4);
             }
 
             tanOfAngle = (playerX - poly2.x) / (playerY - poly2.y);
             oppositeLength = playerY * tanOfAngle;
-            poly3 = new Point2D.Float(playerX - oppositeLength, 0);
+            poly3.setLocation(playerX - oppositeLength, 0);
 
             tanOfAngle = (poly1.x - playerX) / (playerY - poly1.y);
             oppositeLength = playerY * tanOfAngle;
-            poly4 = new Point2D.Float(playerX + oppositeLength, 0);
+            poly4.setLocation(playerX + oppositeLength, 0);
         }
 
         if (poly1 == null) {
-            poly1 = p1;
+            poly1.setLocation(p1);
         }
         if (poly2 == null) {
-            poly2 = p2;
+            poly2.setLocation(p2);
         }
         if (poly3 == null) {
-            poly3 = p3;
+            poly3.setLocation(p3);
         }
         if (poly4 == null) {
-            poly4 = p4;
+            poly4.setLocation(p4);
         }
 
         // draw polygon using points
-        Polygon trapezoid = new Polygon();
+        trapezoid.reset();
         trapezoid.addPoint((int) (poly1.x - handler.getGameCamera().getxOffset()),
                 (int) (poly1.y - handler.getGameCamera().getyOffset()));
         trapezoid.addPoint((int) (poly2.x - handler.getGameCamera().getxOffset()),
